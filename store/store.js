4019a58ce0cb0a4a081eb7a1bb7f06a0dc3e4f3c
DdMad <ddmad1030@gmail.com>
2016-10-27 21:20:03 +0800

    Add example module.

--- store/store.js
+++ store/store.js
@@ -2,7 +2,44 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers/index'
 
 const INITIAL_STATE = {
-  modules: [],
+  modules: [
+    {
+      code: 'CS1010',
+      credit: '4',
+      level: '1',
+      prerequisites: [],
+      lecture: 2,
+      tutorial: 1,
+      lab: 1,
+      project: 3,
+      preparation: 3,
+
+      assignments: [
+        {
+          name: 'Assignment 1',
+          released: '2016-11-01',
+          due: '2016-11-10',
+          percentage: 5,
+          coverage: 3,
+          people: 1
+        },
+        {
+          name: 'Assignment 2',
+          released: '2016-11-11',
+          due: '2016-11-20',
+          percentage: 5,
+          coverage: 5,
+          people: 1
+        }
+      ],
+
+      projects: [],
+      presentations: [],
+      readings: [],
+      tests: [],
+      exams: []
+    }
+  ],
   widgets: []
 }
 
