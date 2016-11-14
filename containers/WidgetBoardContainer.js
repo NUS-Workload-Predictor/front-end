6d5dec926eef91db6aa135d3f7a5419e6b1c0e86
DdMad <ddmad1030@gmail.com>
2016-11-14 19:06:50 +0800

    Map state to widget.

--- containers/WidgetBoardContainer.js
+++ containers/WidgetBoardContainer.js
@@ -1,11 +1,18 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 import WidgetBoard from '../components/WidgetBoard';
 
 class WidgetBoardContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { widgets, dispatch } = this.props;
+
     return (
-      <WidgetBoard />
+      <WidgetBoard widgets={widgets} dispatch={dispatch}  />
     );
   }
 }
@@ -14,4 +21,8 @@ WidgetBoardContainer.propTypes = {
 
 };
 
-export default WidgetBoardContainer;
+const mapStateToProps = (state) => ({
+  widgets: state.widgets
+});
+
+export default connect(mapStateToProps)(WidgetBoardContainer);
