0427403e179e2050c93bd3a0c560b483eae33e27
DdMad <ddmad1030@gmail.com>
2017-02-17 23:15:49 +0800

    Able to check if user is logged in by checking cookie or url parameter

--- components/Header.js
+++ components/Header.js
@@ -7,15 +7,82 @@ import NavigationExpandMore from 'material-ui/svg-icons/navigation/expand-more';
 import ModuleListContainer from '../containers/ModuleListContainer';
 
 import { IVLE_API_KEY, REDIRECT_URL, IVLE_API_BASE_URL } from '../constants/constants';
+
 const ivleLogin = 'https://ivle.nus.edu.sg/api/login/?apikey=' + IVLE_API_KEY + '&url=' + REDIRECT_URL;
+const defaultExpire = 30;
+const tokenName = 'token';
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {logged: false, open: false};
 
+    let token = this.getCookie(tokenName);
+    if (token) {
+      this.state = { ...this.state, logged: true };
+    } else {
+      token = this.getTokenFromUrl();
+      if (token) {
+        this.state = { ...this.state, logged: true };
+        this.saveTokenToCookie(token);
+      }
+    }
+
     this.handleLogin = this.handleLogin.bind(this);
     this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  getTokenFromUrl() {
+    let url = window.location.href;
+    let name = tokenName;
+
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    let results = regex.exec(url);
+
+    if (!results) {
+      return null;
+    }
+
+    if (!results[2]) {
+      return '';
+    }
+
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
+  saveTokenToCookie(token) {
+    const name = tokenName;
+
+    this.removeCookie(name);
+    this.setCookie(name, token, defaultExpire);
+  }
+
+  setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+
+  getCookie(name) {
+    let nameEqual = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEqual) == 0) {
+          return c.substring(nameEqual.length, c.length);
+        }
+    }
+    return null;
+  }
+
+  removeCookie(name) {
+    this.setCookie(name, "", -1);
   }
 
   handleLogin() {
